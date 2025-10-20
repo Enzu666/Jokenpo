304effce60dbeda6f906d8de372e76d1f3467eca
ENZO CREPALDI PEIXOTO <25203666@CFP127ALUNO.LOCAL>
2025-10-20 13:25:27 -0300

    programa finalizado

---
+++ src/game.java
@@ -0,0 +1,101 @@
+import java.util.Random;
+import java.util.Scanner;
+public class game {
+    int escolha1;
+    private Scanner leitor = new Scanner(System.in);
+
+    public void ReceberDados() {
+        System.out.println("---------------------------------");
+        System.out.println("----------**INICIANDO**----------");
+        System.out.println("---------------------------------");
+        System.out.println();
+        System.out.println("Escolha uma opção:");
+        System.out.println();
+        System.out.println("(1) PEDRA");
+        System.out.println("(2) PAPEL");
+        System.out.println("(3) TESOURA");
+        System.out.print("Digite sua escolha: ");
+
+        if (leitor.hasNextInt()) {
+            escolha1 = leitor.nextInt();
+        } else {
+            System.out.println("Entrada inválida. Encerrando rodada.");
+            leitor.next();
+            escolha1 = 0;
+        }
+
+        SituarCondicao();
+    }
+
+    public void SituarCondicao() {
+        if (escolha1 == 1) {
+            System.out.println("você escolheu PEDRA");
+        } else if (escolha1 == 2) {
+            System.out.println("você escolheu PAPEL");
+        } else if (escolha1 == 3) {
+            System.out.println("você escolheu TESOURA");
+        }
+
+        if (escolha1 >= 1 && escolha1 <= 3) {
+            SituarCondicaoDaMaquina();
+        }
+    }
+
+    public void SituarCondicaoDaMaquina() {
+        Random random = new Random();
+
+        int numeroInteiro = random.nextInt(3) + 1;
+
+        System.out.println("máquina escolheu " + numeroInteiro);
+
+        if (escolha1 == 1 && numeroInteiro > 1 && numeroInteiro < 3) {
+            System.out.println("Ah, que pena! você perdeu!");
+
+        } else if (escolha1 == 2 && numeroInteiro == 3) {
+            System.out.println("Ah, que pena! você perdeu!");
+
+        } else if (escolha1 == 3 && numeroInteiro < 2) {
+            System.out.println("Ah, que pena! você perdeu!");
+
+        } else if (escolha1 == 1 && numeroInteiro > 2) {
+            System.out.println("PARABÉNS, VOCÊ GANHOU!");
+
+        } else if (escolha1 == 2 && numeroInteiro == 1) {
+            System.out.println("PARABÉNS, VOCÊ GANHOU!");
+
+        } else if (escolha1 == 3 && numeroInteiro > 1 && numeroInteiro < 3) {
+            System.out.println("PARABÉNS, VOCÊ GANHOU!");
+
+        } else if (escolha1 == 3 && numeroInteiro == 3) {
+            System.out.println("EMPATE");
+
+        } else if (escolha1 == 2 && numeroInteiro == 2) {
+            System.out.println("EMPATE");
+
+        } else if (escolha1 == 1 && numeroInteiro == 1) {
+            System.out.println("EMPATE");
+        }
+
+    }
+
+    public void IniciarJogo() {
+        char continuar;
+        do {
+            ReceberDados();
+
+            System.out.println("---------------------------------");
+            System.out.print("Deseja jogar novamente? (S/N): ");
+
+            String input = leitor.next();
+            if (input.isEmpty()) {
+                continuar = 'N';
+            } else {
+                continuar = Character.toUpperCase(input.charAt(0));
+            }
+
+        } while (continuar == 'S');
+
+        System.out.println("---------------------------------");
+        System.out.println("Jogo encerrado. Até a próxima!");
+    }
+}
---
+++ src/jokenpo.java
@@ -0,0 +1,7 @@
+public class jokenpo {
+    public static void main(String[] args) {
+        game jogo = new game();
+        jogo.IniciarJogo();
+
+    }
+}
